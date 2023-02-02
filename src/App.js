44b3182ee1a43c99db186a0597722e9735b4ee51
Mitsanta_ANDRIAH <103644537+Mitsanta12@users.noreply.github.com>
2023-02-02 15:30:44 +0300

    chore:css&component

--- src/App.js
+++ src/App.js
@@ -1,64 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
+import './nav.css';
+import Todo from './component/todo';
 
-function TodoList() {
-  const [todo, setTodo] = useState('');
-  const [doing, setDoing] = useState([]);
-  const [done, setDone] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setDoing([...doing, todo]);
-    setTodo('');
-  }
-
-  const handleDelete = (index) => {
-    const newDoing = [...doing];
-    newDoing.splice(index, 1);
-    setDoing(newDoing);
-  }
-
-  const handleCheck = (index) => {
-    const currentTodo = doing[index];
-    handleDelete(index);
-    setDone([...done, currentTodo]);
-  }
-
+function App() {
+  
   return (
-    <div className='container'>
-      <form onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          value={todo} 
-          onChange={e => setTodo(e.target.value)} onKeyDown={(e)=>{e.key === 'Enter' && handleSubmit()}}
-          placeholder="Add task"
-        />
-      </form>
-      <div className='todo'>
-        <h3>TODO</h3>
-        <ul>
-          {doing.map((todo, index) => (
-            <li key={index}>
-              {todo}
-              <input 
-                type="checkbox" 
-                onClick={() => handleCheck(index)} 
-              />
-            </li>
-          ))}
-        </ul>
+    <>
+      <div className='row'>
+          <Todo/>  
       </div>
-      <div className='done'>
-        <h3>DONE</h3>
-        <ul>
-          {done.map((todo, index) => (
-            <li key={index}>{todo}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    </>
   );
 }
 
-export default TodoList;
+export default App;
+
+
+
 
---
+++ src/component/todo.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import '../App.css';
+
+function Todo(){
+    const [todo, setTodo] = useState('');
+    const [doing, setDoing] = useState([]);
+    const [done, setDone] = useState([]);
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setDoing([...doing, todo]);
+      setTodo('');
+    }
+
+    const handleDelete = (index) => {
+        const newDoing = [...doing];
+        newDoing.splice(index, 1);
+        setDoing(newDoing);
+      }
+
+    const handleCheck = (index) => {
+        const currentTodo = doing[index];
+        handleDelete(index);
+        setDone([...done, currentTodo]);
+      }
+    
+      const handleDeleteDone = (index) => {
+        const newDone = [...done];
+        newDone.splice(index, 1);
+        setDone(newDone);
+      };
+      
+    return(
+        
+<>
+             <div > 
+                <h1>TODO APPLICATION</h1>
+                <form onSubmit={handleSubmit}>
+                  <input 
+                   type="text" 
+                   value={todo} 
+                   onChange={e => setTodo(e.target.value)} onKeyDown={(e)=>{e.key === 'Enter' && handleSubmit()}}
+                    placeholder="Add task"
+                  />
+                </form>
+              </div>
+            <div className='container'>
+              <div className='todo'>
+                <h3>TODO</h3>
+                <ul>
+                  {doing.map((todo, index) => (
+                    <li key={index}>
+                      {todo}
+                      <input 
+                        type="checkbox" 
+                        onClick={() => handleCheck(index)} 
+                        className='checkbox'
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            
+          <div className='done'>
+            <h3>DONE</h3>
+              <ul>
+                {done.map((todo, index) => (
+                  <li key={index}>
+                    {todo}
+                      <button className='Btn' onClick={() => handleDeleteDone(index)}>X</button>
+                  </li>
+                ))}
+              </ul>
+          </div>
+        
+
+              
+              </div>
+              
+    )
+    
+    </>)    
+}
+
+export default Todo;
